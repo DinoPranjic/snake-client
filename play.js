d1cbabe1fb0a11383241ba81f834940708c07ea2
DinoPranjic <dinopranjic92@gmail.com>
2022-02-22 22:56:18 +0000

    added seperate input module

--- play.js
+++ play.js
@@ -1,22 +1,5 @@
-const { stdin } = require('process');
 const { connect } = require('./client');
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = function(key) {
-  if(key === '\u0003') {
-    process.exit();
-  }
-
-}
-
-stdin.on('data', handleUserInput);
+const { setupInput } = require('./input')
 
 console.log("Connecting ...");
 connect();
