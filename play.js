e6c315ed35de0614e30d8eb43ec31f8b7a166e0f
DinoPranjic <dinopranjic92@gmail.com>
2022-02-22 22:40:07 +0000

    implented basic user input and allowed player to exit game with crtl-c

--- play.js
+++ play.js
@@ -1,4 +1,23 @@
+const { stdin } = require('process');
 const { connect } = require('./client');
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if(key === '\u0003') {
+    process.exit();
+  }
+
+}
+
+stdin.on('data', handleUserInput);
 
 console.log("Connecting ...");
 connect();
+setupInput();
