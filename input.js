6cedab680d404c3f77706a98780c92c1a8f8271c
DinoPranjic <dinopranjic92@gmail.com>
2022-02-23 01:20:36 +0000

    implemented logic to pass command strings to server

--- input.js
+++ input.js
@@ -1,6 +1,11 @@
 const { stdin } = require('process');
+//defining global variable called connection without assigning value
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  //reassigning the value of the variable connection to the entire connect object
+  //all of its methods now available in global scope
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,10 +14,17 @@ const setupInput = function () {
 };
 
 const handleUserInput = function(key) {
-  if(key === '\u0003') {
+  if (key === '\u0003') {
     process.exit();
+  } else if (key === 'w') {
+    connection.write('Move: up');
+  } else if (key === 'a') {
+    connection.write('Move: left');
+  } else if (key === 's') {
+    connection.write('Move: down');
+  } else if (key === 'd') {
+    connection.write('Move: right');
   }
-
 }
 
 stdin.on('data', handleUserInput);
