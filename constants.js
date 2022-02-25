073e3aa822086313d833441de790e53e5ac36365
DinoPranjic <dinopranjic92@gmail.com>
2022-02-25 15:58:09 +0000

    Added player move keys and message keys to a constant variable which is exported and can be shared with input

--- constants.js
+++ constants.js
@@ -1,7 +1,21 @@
 const IP = 'localhost';
 const PORT = 50541;
+const MOVE_KEYS = {
+  "w": "Move: up",
+  "a": "Move: left",
+  "s": "Move: down",
+  "d": "Move: right"
+}
+const PLAYER_MSG_KEYS = {
+  "1": "Say: Haha!",
+  "2": "Say: You suck!",
+  "3": "Say: You\'ll never win!",
+  "9": "Say: THIS. IS. SPARTA!"
+}
 
 module.exports = {
   IP,
-  PORT
+  PORT,
+  MOVE_KEYS,
+  PLAYER_MSG_KEYS
 };
